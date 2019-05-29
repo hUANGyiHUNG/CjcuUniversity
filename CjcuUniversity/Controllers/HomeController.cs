b8559fe55de20df07cb05aa9348afd556c05bc0d
CjcuUniversity <h24661109@mailst.cjcu.edu.tw>
2019-05-29 14:47:55 +0800

    第7次

--- CjcuUniversity/Controllers/HomeController.cs
+++ CjcuUniversity/Controllers/HomeController.cs
@@ -3,24 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CjcuUniversity.DAL;
+using CjcuUniversity.Viewmodel;
+
 
 namespace CjcuUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private SchoolContext db = new SchoolContext();
         public ActionResult Index()
         {
+           
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+    public ActionResult About()
+    {
+            IQueryable<EnrollmentDateGroup> data = from student in db.Students
+                                                   group student by student.EnrollmentDate into dateGroup
+                                                   select new EnrollmentDateGroup()
+                                                   {
+                                                       EnrollmentDate = dateGroup.Key,
+                                                       StudentCount = dateGroup.Count()
+                                                   };
+            return View(data.ToList());
         }
-
-        public ActionResult Contact()
+    protected override void Dispose(bool disposing)
+    {
+        db.Dispose();
+        base.Dispose(disposing);
+    }
+    public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
